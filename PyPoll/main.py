22d65eb0772c476ab4b3bb90e0967b96ad058826
NathanH12 <135286966+NathanH12@users.noreply.github.com>
2023-07-05 17:18:33 -0400

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,39 @@
+import csv
+import os
 
+csv_path = os.path.join("Resources" , "election_data.csv")
+output_path = os.path.join("analysis", "election_analysis.txt")
+
+total_votes = 0
+candidates = {}
+
+with open(csv_path, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)  
+
+    for row in csvreader:
+        total_votes += 1
+        candidate = row[2]
+        candidates[candidate] = candidates.get(candidate, 0) + 1
+
+analysis_results = f"""\
+Election Results
+-------------------------
+Total Votes: {total_votes}
+-------------------------\n"""
+
+for candidate, votes in candidates.items():
+    percentage = (votes / total_votes) * 100
+    analysis_results += f"{candidate}: {percentage:.3f}% ({votes})\n"
+
+winner = max(candidates, key=candidates.get)
+
+analysis_results += f"""\
+-------------------------
+Winner: {winner}
+-------------------------"""
+
+print(analysis_results)
+
+with open(output_path, "w") as output_file:
+    output_file.write(analysis_results)
